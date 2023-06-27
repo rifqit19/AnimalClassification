2e6e8ec531ca4d8415992d2a043202ad6195c6c2
rifqit19 <rifqit19@gmail.com>
2023-06-28 01:23:38 +0700

    Initial Commit

---
+++ AnimalClassification/AnimalClassificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalClassificationApp.swift
+//  AnimalClassification
+//
+//  Created by rifqi triginandri on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalClassificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
